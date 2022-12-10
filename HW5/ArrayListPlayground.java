f14c85aca14feb47787af3cb94cb1ee4de43113f
Arbid-Chowdhury <chowdh28@stockton.edu>
2022-12-09 21:40:00 -0500

    final hw5 commit

--- HW5/ArrayListPlayground.java
+++ HW5/ArrayListPlayground.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class ArrayListPlayground {
     public static void main(String[] args) {
         System.out.println("Start of HW5");
+
         // Question 1
         // Write Java statements that do the following:
         // a) Declare an arrayList nums of 15 elements of type Integer.
@@ -18,12 +19,48 @@ public class ArrayListPlayground {
         // d) Set the value of the 9th element of the array nums to the sum of the 13th
         // and 2nd elements of the array nums.
 
+
+
+        //q: make an arraylist of 15 elements
+        // ArrayList<Integer> nums = new ArrayList<Integer>(15);
+
+
+
+
+        ArrayList<Integer> nums = new ArrayList<Integer>(15);
+        // fill the arraylist nums with 0's
+        for (int i = 0; i < 15; i++) {
+            nums.add(0);
+        }
+        System.out.println(nums.get(10));
+        nums.set(4, 99);
+        nums.set(12, 15);
+        nums.set(1, 6);
+        nums.set(8, nums.get(12) + nums.get(1));
+        System.out.println(nums.get(9));
+        System.out.println(nums);
+
         // Question 2
         // a) create an arrayList of Strings that contain each day of the week.(starting
         // on monday)
         // b) output each of the days of the week
         // c) output the days of the week that we have class
         // d) change the arrayList to start on Sunday
+        ArrayList<String> days = new ArrayList<String>(7);
+        for (int i = 0; i < 7; i++) {
+            days.add("");
+        }
+        days.set(0, "Monday");
+        days.set(1, "Tuesday");
+        days.set(2, "Wednesday");
+        days.set(3, "Thursday");
+        days.set(4, "Friday");
+        days.set(5, "Saturday");
+        days.set(6, "Sunday");
+        System.out.println(days);
+        System.out.println(days.get(1) + " and " + days.get(3));
+        Collections.rotate(days, 1);
+        System.out.println(days);
 
         // Question 3
         // a) create an ArrayList and prompt the user for numbers to add to it until the
@@ -56,6 +93,58 @@ public class ArrayListPlayground {
         // System.out.println();
         // }
         // }
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Scanner input = new Scanner(System.in);
+        while (numbers.size() < 9) {
+            System.out.println("Enter a number: ");
+            int num = input.nextInt();
+            numbers.add(num);
+            if (num == 0) {
+                if (numbers.size()< 9){
+                    System.out.println("Please enter " + (9 - numbers.size()) + "more numbers to create the matrix...");
+                    Collections.sort(numbers);
+                    System.out.println(numbers);
+                    System.out.println("The smallest value is " + numbers.get(0));
+                    System.out.println("The largest value is " + numbers.get(numbers.size() - 1));
+                }
+                else if (numbers.size() == 9){
+                Collections.sort(numbers);
+                System.out.println(numbers);
+                System.out.println("The smallest value is " + numbers.get(0));
+                System.out.println("The largest value is " + numbers.get(numbers.size() - 1));
+                }
+            }
+            else if (num != 0) {
+                Collections.sort(numbers);
+                System.out.println(numbers);
+                System.out.println("The smallest value is " + numbers.get(0));
+                System.out.println("The largest value is " + numbers.get(numbers.size() - 1));
+            }
 
-    }
-}
+            for(int i=0;i<numbers.size();i++)
+            {
+             System.out.print(numbers.get(i)+" ");
+            if((i+1)%3==0)
+            {
+             System.out.println();
+            }
+            }
+            }
+        }
+
+
+        //for (int i = 0; i < 9; i++) {
+           // System.out.println("Enter a number: ");
+            //int num = input.nextInt();
+            //numbers.add(num);
+            //if (num == 0){
+              //  break;
+            //}
+            //if (numbers.size() < 9) {
+            //System.out.println("Please enter " + (9 - numbers.size()) + "more numbers to create the matrix...");
+
+
+        }
+    
+
+    
